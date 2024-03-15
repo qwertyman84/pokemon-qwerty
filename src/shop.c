e56099860c60981b22fdea97736d34f59fe34298
mudskipper13 <MariaJumpMan001@hotmail.com>
2024-03-15 15:11:46 +0700

    Merge branch 'me/shop-ui' into shop-ui

--- src/shop.c
+++ src/shop.c
@@ -1213,7 +1213,8 @@ static void Task_BuyMenuTryBuyingItem(u8 taskId)
             return;
         }
     }
-    else if (!IsEnoughMoney(&gSaveBlock1Ptr->money, sShopData->totalCost))
+
+    if (!IsEnoughMoney(&gSaveBlock1Ptr->money, sShopData->totalCost))
     {
         PlaySE(SE_BOO);
         gTasks[taskId].data[0] = 70;
