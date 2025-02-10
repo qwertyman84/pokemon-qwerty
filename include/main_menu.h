2fff00ae1ef7ede6ae1c3421201cddd86b3a7461
qwertyman84 <qwertyman8428.5@gmail.com>
2025-02-09 16:01:16 -0800

    Revert "Merge branch 'main_menu' of https://github.com/TeamAquasHideout/pokeemerald"
    This reverts commit 5bc14121bd3babc016cd50356a029680cc26b217, reversing
    changes made to 89e16864153c0e06a8383e2ead9b70c334ec905d.

--- include/main_menu.h
+++ include/main_menu.h
@@ -3,8 +3,6 @@
 
 void CB2_InitMainMenu(void);
 void CreateYesNoMenuParameterized(u8 x, u8 y, u16 baseTileNum, u16 baseBlock, u8 yesNoPalNum, u8 winPalNum);
-void CB2_ReinitMainMenu(void);
-void CB2_NewGameBirchSpeech_FromNewMainMenu(void);
 void NewGameBirchSpeech_SetDefaultPlayerName(u8);
 
 #endif // GUARD_MAIN_MENU_H
